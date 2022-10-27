af5e7055488ce2bae9996d5bf2b80a2aa9ccc60c
MasterxDual <agustinbram@gmail.com>
2022-10-26 22:40:30 -0300

    Creating our library and our principal files

---
+++ library.h
@@ -0,0 +1,8 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+char menu(void);
+
+#include "functions_library.cpp"
+
+#endif
---
+++ principal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "library.h"
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
